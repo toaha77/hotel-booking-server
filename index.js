e3689a2c4c801242c9d0d18e8ad42100f08f92a2
toaha77 <istiaktoaha07@gmail.com>
2024-02-03 23:34:00 +0600

    room data updated

--- index.js
+++ index.js
@@ -3,6 +3,35 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://hotel-manage:evNvEoEzYve2OJDG@cluster0.pua1xq5.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const roomCollection = client.db("hotel-manageDB").collection('rooms');
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
